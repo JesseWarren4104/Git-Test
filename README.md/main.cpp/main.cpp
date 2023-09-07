59f89adad780a1983f08faa0b1068fcd4da997cc
JesseWarren4104 <jwarre46@ut.utm.edu>
2023-09-07 14:18:37 -0500

    Added new code

--- README.md/main.cpp/main.cpp
+++ README.md/main.cpp/main.cpp
@@ -1,11 +1,28 @@
-
 #include <iostream>
 
 using namespace std;
 
+int sum(int n){
+  int accumulator = 0;
+
+  for(int i = 1; i <= n; i++){
+    accumulator += i;
+  }
+  
+  return accumulator;
+}
+
 int main() {
 
-  cout << "Hello World!" << endl;
+  cout << "Enter a number: ";
+  int n; cin >> n; cout << endl;
+
+  int answer = sum(n);
+  
+  cout << "The sum of the numbers from 1 to " << n << " is: " << answer << endl;
   
   return 0;
 }
+
+
+  
