fd07b37ae53f6c808c0397cd102465ecb4dfe242
JesseWarren4104 <jwarre46@ut.utm.edu>
2023-09-07 17:53:45 -0500

    Added product function.

--- README.md/main.cpp/main.cpp
+++ README.md/main.cpp/main.cpp
@@ -12,14 +12,25 @@ int sum(int n){
   return accumulator;
 }
 
+int product(int n){
+  int accumulator = 1;
+
+  for(int i = 1; i <= n; i++){
+    accumulator *= i;
+  }
+
+  return accumulator;
+}
+
 int main() {
 
   cout << "Enter a number: ";
   int n; cin >> n; cout << endl;
 
   int answer = sum(n);
-  
+  int answer2 = product(n);
   cout << "The sum of the numbers from 1 to " << n << " is: " << answer << endl;
+  cout << "The product of the numbers from 1 to " << n << " is: " << answer2 << endl;
   
   return 0;
 }
